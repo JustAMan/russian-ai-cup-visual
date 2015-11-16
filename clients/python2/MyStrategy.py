71e1641208dbd709389d4ce84dc0e34ba28f7572
Vasily <taimen@nm.ru>
2015-11-16 15:39:53 +0300

    Improved python debug client a bit

--- clients/python2/MyStrategy.py
+++ clients/python2/MyStrategy.py
@@ -3,6 +3,11 @@ from model.Game import Game
 from model.Move import Move
 from model.World import World
 
+try:
+    from debug_client import Color
+except ImportError:
+    pass
+
 class MyStrategy:
     def __init__(self):
         try:
@@ -11,6 +16,7 @@ class MyStrategy:
             self.debug = None
         else:
             self.debug = DebugClient()
+            self.GREEN = Color(r=0.0, g=1.0, b=0.0)
 
     def move(self, me, world, game, move):
         """
@@ -20,7 +26,12 @@ class MyStrategy:
         @type move: Move
         """
         if self.debug:
-            self.debug.try_me(world, game)
+            with self.debug.pre() as dbg:
+                dbg.circle(100.0, 100.0, 50.0, (1.0, 0.0, 0.0))
+            with self.debug.post() as dbg:
+                dbg.circle(game.world_width * game.track_tile_size - 100,
+                           game.world_height * game.track_tile_size - 100,
+                           50.0, self.GREEN)
 
         move.engine_power = 1.0
         move.throw_projectile = True
--- clients/python2/debug_client.py
+++ clients/python2/debug_client.py
@@ -1,20 +1,51 @@
 import socket
+import collections
+
+Color = collections.namedtuple('Color', 'r g b')
 
 class DebugClient:
     DEFAULT_HOST = '127.0.0.1'
     DEFAULT_PORT = 13579
+
+    MODE_PRE, MODE_POST, MODE_UNKNOWN = 'pre post unknown'.split()
+    BEGINS = {MODE_PRE: 'begin pre\n', MODE_POST: 'begin post\n'}
+    ENDS = {MODE_PRE: 'end pre\n', MODE_POST: 'end post\n'}
     def __init__(self, host=None, port=None):
         self.socket = socket.socket()
         self.socket.setsockopt(socket.IPPROTO_TCP, socket.TCP_NODELAY, True)
         self.socket.connect((host or self.DEFAULT_HOST, port or self.DEFAULT_PORT))
+        self.mode = self.MODE_UNKNOWN
 
-    def try_me(self, world, game):
-        self.socket.sendall('begin pre\n')
-        self.socket.sendall('circle 100.0 100.0 50.0 1.0 0.0 0.0\n')
-        self.socket.sendall('end pre\n')
+    def pre(self):
+        assert self.mode == self.MODE_UNKNOWN
+        self.mode = self.MODE_PRE
+        return self
 
-        self.socket.sendall('begin post\n')
-        self.socket.sendall('circle %f %f 50.0 0.0 1.0 0.0\n' % \
-                (game.world_width * game.track_tile_size - 100,
-                 game.world_height * game.track_tile_size - 100))
-        self.socket.sendall('end post\n')
+    def post(self):
+        assert self.mode == self.MODE_UNKNOWN
+        self.mode = self.MODE_POST
+        return self
+
+    def __enter__(self):
+        assert self.mode in self.BEGINS
+        self.socket.sendall(self.BEGINS[self.mode])
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        assert self.mode in self.ENDS
+        self.socket.sendall(self.ENDS[self.mode])
+        self.mode = self.MODE_UNKNOWN
+
+    @staticmethod
+    def __make_color(color):
+        if isinstance(color, Color):
+            return color
+        if isinstance(color, (tuple, list)) and len(color) >= 3:
+            return Color(r=color[0], g=color[1], b=color[2])
+        return color
+
+    def circle(self, x0, y0, r0, color):
+        assert self.mode != self.MODE_UNKNOWN
+        color = self.__make_color(color)
+        self.socket.sendall('circle %f %f %f %f %f %f\n' % (x0, y0, r0,
+                                                            color.r, color.g, color.b))
