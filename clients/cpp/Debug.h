5ba93367b3e73e82825440b090451a07db37f694
egosha <Egor.Shakh@transas.com>
2015-11-20 21:01:08 +0300

    cpp-client

---
+++ clients/cpp/Debug.h
@@ -0,0 +1,29 @@
+#include <cstdlib>
+#include <cstdio>
+#include <string>
+
+class Debug
+{
+public:
+	static std::string DEFAULT_HOST;
+	static std::string DEFAULT_PORT;
+	static const int BUF_SIZE;
+
+	Debug();
+	void beginPre();
+	void endPre();
+	void beginPost();
+	void endPost();
+	void circle(double x, double y, double r, int32_t color = 0xFF0000);
+	void fillCircle(double x, double y, double r, int32_t color = 0xFF0000);
+	void rect(double x1, double y1, double x2, double y2, int32_t color = 0x00FF00);
+	void fillRect(double x1, double y1, double x2, double y2, int32_t color = 0x00FF00);
+	void line(double x1, double y1, double x2, double y2, int32_t color = 0x0000FF);
+	void text(double x, double y, const char* text, int32_t color = 0x000000);
+	
+private:
+	void sendCommand(const char* str);
+	void writeWithColor(char* buf, int32_t color);
+
+	int sfd;
+};
